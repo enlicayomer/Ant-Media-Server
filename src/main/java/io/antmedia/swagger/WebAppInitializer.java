002c6c20390b5509929fb1684efe2386c144a607
omer enlicay <omer.enlicay@detaysoft.com>
2018-09-13 16:04:55 +0300

    swagger configuration init

---
+++ src/main/java/io/antmedia/swagger/WebAppInitializer.java
@@ -0,0 +1,20 @@
+/*
+package io.antmedia.swagger;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { SwaggerWebConfig.class };
+    }
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/*" };
+    }
+}
+*/
